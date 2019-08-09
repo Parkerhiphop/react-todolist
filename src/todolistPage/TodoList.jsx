a440a6096fbdd70706cd4906a1d0379606ee04c4
張丞逸 <parkerhiphop027@gmail.com>
2019-08-09 18:17:00 +0800

    finish input and btn

--- src/todolistPage/TodoList.jsx
+++ src/todolistPage/TodoList.jsx
@@ -1,27 +1,120 @@
 // @flow
 
-import React from 'react';
+import React, {useState} from 'react';
+
+const styles = {
+  wrapper: {
+    display: 'grid',
+    gridTemplateRows: '100px 80px 500px',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 30,
+  },
+  inputWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    width: 300,
+    height: 40,
+    borderRadius: 5,
+    padding: '0px 0px 0px 10px',
+    margin: '0px 30px 0px 0px',
+    border: 'none',
+  },
+  button: {
+    width: 60,
+    height: 40,
+    border: 'none',
+    borderRadius: 5,
+    fontSize: 18,
+  },
+  footer: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '20px 0px 0px 0px',
+  },
+  taskWrapper: {
+    border: 'solid 1px #777',
+    borderRadius: 10,
+    width: 400,
+    height: 35,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: '5px 0px',
+  },
+  task: {
+    fontSize: 15,
+  },
+}
 
 type Props = {
+  fakeTodos: Array,
   todos: Array,
+  setTodos: Function,
 };
 
 function TodoList({
+  fakeTodos,
   todos,
+  setTodos,
 }: Props) {
-    return (
-    <div>
+
+  const [test, setTest] = useState('');
+
+  return (
+    <div style={styles.wrapper}>
+      <h1 style={styles.title}>
+        Todo({todos.length})
+      </h1>
+      <div style={styles.inputWrapper}>
+        <input
+          style={styles.input}
+          type="text"
+          value={test}
+          onKeyUp={(e) => {
+            if (e.which === 13 || e.keyCode ===13) {
+              todos.push(test);
+              setTest('');
+            }
+            // console.log('push', todos.push('1'));
+            console.log('todos', todos);
+            console.log('value', e.target.value);
+            console.log('test', test);
+            // console.log('push', todos.push(test));
+          }}
+          onChange={(e) => {setTest(e.target.value)}}
+          placeholder="你想什麼沒做？" />
+        <button
+        style={styles.button}
+        type="button"
+        onClick={() => {
+          todos.push(test);
+          setTest('');
+        }}>
+        新增
+        </button>
+      </div>
+      <div style={styles.footer}>
         {todos.map((t, i) => (
           <div
+           style={styles.taskWrapper}
            key={i}>
-            <h4>
+            <span style={styles.task}>
                 {t}
-            </h4>
-            <hr />
+            </span>
           </div>
         ))}
+      </div>
     </div>
-    );
-};
+  );
+}
 
 export default TodoList;
--- src/todolistPage/TodoListPage.jsx
+++ src/todolistPage/TodoListPage.jsx
@@ -1,14 +1,12 @@
 // @flow
 
-import React from 'react';
-import TodoInput from './TodoInput';
+import React, {useState} from 'react';
 import TodoList from './TodoList';
-import TodoListTitle from './TodoListTitle';
 
 const styles = {
   wrapper: {
-    width: 500,
-    height: 600,
+    width: 600,
+    height: 680,
     border: 'solid 3px #777',
     borderRadius: 10,
   },
@@ -16,15 +14,15 @@ const styles = {
 
 function TodoListPage() {
   
-  const todos = ['完成TodoList', '用Class做一次', 'push到Github'];
+  const fakeTodos = ['完成TodoList', '用Class做一次', 'push到Github'];
+  const [todos, setTodos] = useState([]);
+  console.log('todos', todos);
 
   return (
     <div style={styles.wrapper}>
-      <TodoListTitle
-       todos={todos} />
-      <TodoInput />
       <TodoList
-       todos={todos} />
+        todos={todos}
+        setTodos={setTodos} />
     </div>
   );
 }
